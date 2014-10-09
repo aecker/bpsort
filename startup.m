ed2ea7a30cad3346bb1a4c29deb9a3a8f583d264
Alexander Ecker <alexander.ecker@uni-tuebingen.de>
2014-10-09 14:40:13 +0200

    Added startup script

---
+++ startup.m
@@ -0,0 +1,15 @@
+% startup script for BP library
+
+p = fileparts(mfilename('fullpath'));
+addpath(p)
+addpath(fullfile(p, 'util'))
+addpath(fullfile(p, 'plot'))
+addpath(fullfile(p, 'data'))
+
+p = fileparts(p);
+moksm = fullfile(p, 'moksm');
+if exist(moksm, 'file')
+    addpath(moksm)
+else
+    warning('Could not find moksm repo, which is required for initialization!')
+end
